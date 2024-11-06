e3428b1040ec28c912c9ce188943f0b1d382b7cd
gabinacodes <badruraidah@gmail.com>
2024-11-06 17:33:13 +0100

    first commit and creation of header plus adding landing page image

---
+++ src/Component/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from '../pics/image'
+function Header() {
+  return (
+    <div>
+        <div>
+            <img src={Image.logo} alt="logo"/>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pics/image.js
@@ -0,0 +1,23 @@
+import boost from './boost.png'
+import certificate1 from './certificate1.svg'
+import certificate2 from './certificate2.svg'
+import certificate3 from './certificate3.svg'
+import certificate4 from './certificate4.svg'
+import certificate5 from './certificate5.png'
+import certificate6 from './certificate6.svg'
+import certificate7 from './certificate7.svg'
+import certificateWithLaptop from './certificates-with-laptop.png'
+import choose from './choose-template.svg'
+import design from './design-template.svg'
+import facebook from './devicon_facebook.svg'
+import instagram from './devicon_instagram-icon.svg'
+import linkedin from './devicon_linkedin.svg'
+import X from './devicon_X.svg'
+import download from './download&share.svg'
+import generate from './generate-template.svg'
+import logo from './logo.svg'
+import recognization from './recognition.svg'
+const Image ={
+boost,certificate1,certificate2,certificate3,certificate4,certificate5,certificate6,certificate7,certificateWithLaptop,choose,design,facebook,instagram,linkedin,X,download,generate,logo,recognization
+}
+export default Image
